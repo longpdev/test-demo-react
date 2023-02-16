5e83ba9c89a79cdb2c892de9e76c2e342ad16420
Long <longpdev@gmail.com>
2023-02-15 21:11:19 -0600

    16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+    render() {
+        //JSX
+        return (
+            <div>
+                my firstadsadas component
+                {Math.random()}
+            </div>
+
+
+        );
+    }
+}
+
+export default MyComponent;
