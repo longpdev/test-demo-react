ba32673bbe3bc61850d259739cd9c6bfec645058
Long <longpdev@gmail.com>
2023-02-19 01:03:09 -0600

    21. state

--- src/App.js
+++ src/App.js
@@ -3,27 +3,53 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import React from 'react';
 
-  return (
-    <div>
-      Hello World
-      <MyComponent></MyComponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hello World
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+class App extends React.Component {
+
+  state = {
+    name: "231231232131231",
+    address: '1805',
+    age: 23
+  };
+
+  abc = {
+    name: "123"
+  }
+
+  render() {
+    return (
+      <div>
+        My name is {this.state.name} and I'm from {this.state.address}
+        My name is {this.abc.name}
+      </div>
+    )
+  }
 }
+
+// const App = () => {
+//   return (
+
+//   );
+// }
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello World
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
